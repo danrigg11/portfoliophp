feccb92af0ece560d8ff14ee2442cf39eb1042f7
Daniel <dan_rigg11@hotmail.com>
2018-10-11 16:40:10 +0100

    first commit

---
+++ laravelportfolio/resources/views/index.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/master.css">
+
+        <title>Laravel</title>
+
+
+    </head>
+    <body>
+        <!-- HOME -->
+
+
+        <section>
+          <div class="title">
+            <h1>Hello World</h1>
+          </div>
+          <div>
+            <canvas class="canvas" id="space_box" style="background:#000;"></canvas>
+          </div>
+
+        </section>
+        <!-- ABOUT -->
+        <section>
+
+        </section>
+        <!-- PORTFOLIO -->
+        <section>
+
+        </section>
+        <!-- BLOG -->
+        <section>
+
+        </section>
+        <!-- CONTACT -->
+        <section>
+
+        </section>
+        <footer></footer>
+        <script src="/js/canvas.js"></script>
+    </body>
+</html>
